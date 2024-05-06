1c6721bebcaba35be75e4a273a64981a71ed411c
Andrii Zahorodnii <zaho@mit.edu>
2024-05-06 12:29:54 -0400

    fix memory gpu request

--- run_train_rnn.sh
+++ run_train_rnn.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem-per-cpu=12G         # memory per cpu-core (4G is default)
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=high-capacity
-#SBATCH --constraint=11GB
+#SBATCH --mem=11GB
 #SBATCH -t 10:00:00          # total run time limit (HH:MM:SS)
 #SBATCH --array=0-19
 #SBATCH --output /om2/user/zaho/flappydream/reports/slurm-%A_%a.out # STDOUT
@@ -12,7 +12,7 @@ export PATH="/om2/user/zaho/anaconda3/bin:$PATH"
 A=$((SLURM_ARRAY_TASK_ID/5))
 B=$((SLURM_ARRAY_TASK_ID%5))
 arrA=("0" "5" "10" "20")
-arrB=(5 6 7 8 9)
+arrB=(10 11 12 13 14)
 A=${arrA[$A]}
 B=${arrB[$B]}
 
