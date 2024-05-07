a16b3c1134ad2de1187f50f3a061325d76b6fc3b
Andrii Zahorodnii <zaho@mit.edu>
2024-05-06 21:37:22 -0400

    change to 20 epoch roll back

--- train_rnn.py
+++ train_rnn.py
@@ -189,8 +189,7 @@ def train_rnn(model, training_data, n_epochs, optimizer, save_every_epochs=50, v
                 optimizer.load_state_dict(optimizer_state)
 
                 # Restore model and optimizer states
-                epoch_states.pop(-1)
-                checkpoint = epoch_states[-1]
+                checkpoint = epoch_states[0]
 
                 epoch = checkpoint['epoch']+1
                 model.load_state_dict(checkpoint['model_state_dict'])
@@ -210,14 +209,13 @@ def train_rnn(model, training_data, n_epochs, optimizer, save_every_epochs=50, v
             continue
 
         # Save model and optimizer state every epoch
-        if (epoch+1) % 10 == 0:
-            epoch_states.append({
-                'epoch': epoch,
-                'model_state_dict': copy.deepcopy(model.state_dict()),
-                'optimizer_state_dict': copy.deepcopy(optimizer.state_dict())
-            })
-            while len(epoch_states) > 10:
-                epoch_states.pop(0)
+        epoch_states.append({
+            'epoch': epoch,
+            'model_state_dict': copy.deepcopy(model.state_dict()),
+            'optimizer_state_dict': copy.deepcopy(optimizer.state_dict())
+        })
+        while len(epoch_states) > 20:
+            epoch_states.pop(0)
 
         if ((epoch+1) % note_every_epochs == 0):
             epoch_end_time = time.time()
