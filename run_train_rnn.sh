130c076bd59450e9d49233b687ae14a422ab9fd0
Andrii Zahorodnii <zaho@mit.edu>
2024-05-07 08:34:27 -0400

    change lambda vals

--- run_train_rnn.sh
+++ run_train_rnn.sh
@@ -10,7 +10,7 @@ export PATH="/om2/user/zaho/anaconda3/bin:$PATH"
 
 A=$((SLURM_ARRAY_TASK_ID/5))
 B=$((SLURM_ARRAY_TASK_ID%5))
-arrA=("0" "5" "10" "20")
+arrA=("0" "10" "20" "40")
 arrB=(20 21 22 23 24 25)
 A=${arrA[$A]}
 B=${arrB[$B]}
