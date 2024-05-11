ac4c882ff31dd836c65e09251248f70bccc7f37d
Andrii Zahorodnii <zaho@mit.edu>
2024-05-11 09:15:07 -0400

    more openmind stuff

--- run_night.sh
+++ run_night.sh
@@ -4,7 +4,7 @@
 #SBATCH --constraint=high-capacity,11GB
 #SBATCH --mem=11GB
 #SBATCH -t 3:00:00          # total run time limit (HH:MM:SS)
-#SBATCH --array=0-239
+#SBATCH --array=0-119
 #SBATCH --output /om2/user/zaho/flappydream/reports/slurm-%A_%a.out # STDOUT
 export PATH="/om2/user/zaho/anaconda3/bin:$PATH"
 
@@ -12,7 +12,7 @@ A=$((SLURM_ARRAY_TASK_ID%6))
 B=$(((SLURM_ARRAY_TASK_ID/6)%20))
 C=$(((SLURM_ARRAY_TASK_ID/6)/20))
 arrA=("0" "4" "8" "16" "32" "64")
-arrB=(20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39)
+arrB=(40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59)
 arrC=("9 10 11" "3 4 5 9 10 11")
 A=${arrA[$A]}
 B=${arrB[$B]}
